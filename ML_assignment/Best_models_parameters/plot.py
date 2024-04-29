f612ee3d7ed1c0b7e3447aa1b0390672bf3e5cf8
rafyel01 <rafaelveziryan@gmail.com>
2024-04-29 11:30:23 +0000

    changed plots

--- ML_assignment/Best_models_parameters/plot.py
+++ ML_assignment/Best_models_parameters/plot.py
@@ -2,17 +2,17 @@ import pickle
 import matplotlib.pyplot as plt
 import numpy as np
 
-with open("transformer_best_plot_val_loss", "rb") as f:
+with open("transformer_best_plot_tr_loss", "rb") as f:
     tr_loss = pickle.load(f)
 
 
-with open("transformer_best_plot_val_perp", "rb") as f:
+with open("transformer_best_plot_tr_perp", "rb") as f:
     tr_perp = pickle.load(f)
 
 x = np.arange(1, 101)
 
-fig, (ax1, ax2) = plt.subplots(1, 2)
-fig.suptitle('Validation Loss and perplexity curves for Transformer')
+fig, (ax1, ax2) = plt.subplots(1, 2, figsize = (15, 8))
+fig.suptitle('Training Loss and perplexity curves for Transformer')
 ax1.set_xlabel("Epochs")
 ax2.set_xlabel("Epochs")
 ax1.set_ylabel("Loss")
@@ -22,4 +22,4 @@ ax2.set_title("Perplexity vs Epochs")
 ax1.plot(x, tr_loss)
 ax2.plot(x, tr_perp)
 
-fig.savefig("Transformer_valid_loss_perp.png")
+fig.savefig("Transformer_train_loss_perp.png")
